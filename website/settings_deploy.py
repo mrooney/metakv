5f9dfe9f51be831b2bdae8e9de2ec228aa915457
Michael Rooney <mrooney.github@rowk.com>
2014-02-23 23:58:26 -0600

    fresh ports

--- website/settings_deploy.py
+++ website/settings_deploy.py
@@ -1,15 +1,16 @@
+GUNICORN_PORT = 32001
 SERVICES = {
     "nginx":
         {
-            "port": 25684,
-            "gunicorn_port": 14449,
+            "port": 32000,
+            "gunicorn_port": GUNICORN_PORT,
             "templates": ["{project_dir}/nginx.conf.template"],
             "start": "nginx -c {project_dir}/nginx.conf",
             "restart": "kill -s SIGHUP {pid}",
         },
     "gunicorn":
         {
-            "port": 14449,
+            "port": GUNICORN_PORT,
             "templates": ["{project_dir}/settings_gunicorn.py.template"],
             "before": "./before_deploy.sh",
             "start": "gunicorn -D -c settings_gunicorn.py metakv.wsgi:application",
